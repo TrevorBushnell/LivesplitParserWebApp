299b2dd762af344140d2c6cdb28fe3b6c4552538
[Henry} <[hsfencer812@gmail.com]>
2024-11-02 17:11:20 -0700

    new stuff

--- ParserApp.py
+++ ParserApp.py
@@ -1,21 +1,27 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 from livesplit_parser import LivesplitData
+from livesplit_parser import RunnerData
 import tempfile
 
 st.title("Livesplit Parser Web App")
 
 st.write("testing all library functions")
-my_run = None
+runner_dict = RunnerData({})
 
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is not None:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.read())
-        temp_file_path = tmp_file.name
-    
-    my_run = LivesplitData(temp_file_path, time_key='RealTime')
+uploaded_files = st.file_uploader(
+    "Upload LLS files", accept_multiple_files=True
+)
+count = 1
+for uploaded_file in uploaded_files:
+    my_run = None
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.read())
+            temp_file_path = tmp_file.name
+        
+        my_run = LivesplitData(temp_file_path, time_key='RealTime')
+    runner_dict.add_runner_data("runner"+str(count), my_run)
     
     # Display the content or process it
 # my_run = LivesplitData(uploaded_file, time_key="RealTime")
